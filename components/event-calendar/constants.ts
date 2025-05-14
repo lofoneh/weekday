b1b24f6dba92b2eb4d8eb7fb1ce01c8b6718a3d5
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-05-14 15:56:05 +0000

    Adjust calendar view parameters to focus on business hours

--- components/event-calendar/constants.ts
+++ components/event-calendar/constants.ts
@@ -4,14 +4,17 @@ export const EventHeight = 24;
 export const EventGap = 4;
 
 // Height of hour cells in week and day views - controls the scale of time display
-export const WeekCellsHeight = 48;
+// export const WeekCellsHeight = 48;
+export const WeekCellsHeight = 75;
 
 // Number of days to show in the agenda view
 export const AgendaDaysToShow = 30;
 
 // Start and end hours for the week and day views
-export const StartHour = 0;
-export const EndHour = 24;
+// export const StartHour = 0;
+// export const EndHour = 24;
+export const StartHour = 6;
+export const EndHour = 21;
 
 // Default start and end times
 export const DefaultStartHour = 9; // 9 AM
