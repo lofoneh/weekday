6af575b55e7a1ad7c1500c9fcc8069c8bdb69098
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-05-16 09:23:12 +0000

    cleanup

--- server/auth/auth-client.ts
+++ server/auth/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 const authClient = createAuthClient();
 
-export const { listAccounts, signIn, signOut, useSession } = authClient;
+export const { signIn, signOut, useSession } = authClient;
 
 export type Session = typeof authClient.$Infer.Session;
